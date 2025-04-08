21ecb03dc28ac01040cdde7c7ce9940c33b3c24d
root <root@localhost.localdomain>
2025-04-08 15:23:18 -0500

    vista de especialistas

--- src/app/components/professionals/professionals.component.ts
+++ src/app/components/professionals/professionals.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { GlobalService } from '../../services/global.service';
+import { RealtimeProfesionalesService } from '../../services/realtime-profesionales.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-professionals',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './professionals.component.html',
   styleUrl: './professionals.component.css'
 })
 export class ProfessionalsComponent {
+  viewMode: 'grid' | 'list' = 'grid';
 
+  constructor(
+    public global: GlobalService,
+    public professionalsService: RealtimeProfesionalesService
+  ) { }
+
+  setViewMode(mode: 'grid' | 'list') {
+    this.viewMode = mode;
+  }
 }
