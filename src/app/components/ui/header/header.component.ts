4f78867c621035bc5f434e9903b79434337ba671
root <root@localhost.localdomain>
2025-04-08 06:34:03 -0500

    inicio de sesion y cierre de secion

--- src/app/components/ui/header/header.component.ts
+++ src/app/components/ui/header/header.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { GlobalService } from '../../../services/global.service';
-
+import Swal from 'sweetalert2';
+import { AuthPocketbaseService } from '../../../services/auth-pocketbase.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 constructor (
-  public global: GlobalService
+  public global: GlobalService,
+  public auth: AuthPocketbaseService
 ) {}
+logoutUser() {
+  Swal.fire({
+    title: '¿Quieres cerrar sesión?',
+    text: "",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    confirmButtonText: 'Sí, cerrar sesión',
+    cancelButtonText: 'Mantenerme aqui'
+    
+  }).then((result) => {
+    if (result.isConfirmed) {
+      this.auth.logoutUser(); // Call the original logout method
+      Swal.fire(
+        '¡Cerrado!',
+        'Has cerrado sesión con éxito.',
+        'success'
+      );
+    }
+  });
+}
 }
